738f5c6ba94eb42a608214600f81f9f7f0558b26
I-am-aman <readyaman@gmail.com>
2019-12-25 00:49:29 +0530

    Player movement added

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import math
 
 # Initialize the game
 pygame.init()
@@ -35,3 +36,31 @@ while 1:
             # if it is quit the game
             pygame.quit()
             exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_w:
+                keys[0] = True
+            elif event.key == K_a:
+                keys[1] = True
+            elif event.key == K_s:
+                keys[2] = True
+            elif event.key == K_d:
+                keys[3] = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                keys[0] = False
+            elif event.key == pygame.K_a:
+                keys[1] = False
+            elif event.key == pygame.K_s:
+                keys[2] = False
+            elif event.key == pygame.K_d:
+                keys[3] = False
+
+    # Move player
+    if keys[0]:
+        playerpos[1] -= 5
+    elif keys[2]:
+        playerpos[1] += 5
+    if keys[1]:
+        playerpos[0] -= 5
+    elif keys[3]:
+        playerpos[0] += 5
